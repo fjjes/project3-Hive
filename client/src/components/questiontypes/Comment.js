e9d90e47f8848b60625cb55a9d9de276f9e9b6ce
Nikita George <nikkigeorge2@gmail.com>
2021-06-21 13:01:49 -0600

    Fixed submit button

--- client/src/components/questiontypes/Comment.js
+++ client/src/components/questiontypes/Comment.js
@@ -1,31 +1,32 @@
-import React, {useState} from 'react'
-import TextField from '@material-ui/core/TextField'
-
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
 
 function Comment() {
-    const [comment, setComment] = useState("")
+  const [comment, setComment] = useState("");
 
-    const handleChange = (e) => {
-        setComment(e.target.value)
-        console.log (comment)
-    }
-    return (
-        <div>
-            <p>If you wish to add additional comments, you can enter them below:</p>
-            <TextField 
-            id="outlined-basic" 
-            label="comment" 
-            variant="outlined"
-            multiline
-            value={comment}
-            onChange={handleChange}
-            />
-        <div>
-        <button>submit</button>
-        </div>
-        </div>
-
-    )
+  const handleChange = (e) => {
+    setComment(e.target.value);
+  };
+  return (
+    <div>
+      <p>If you wish to add additional comments, you can enter them below:</p>
+      <TextField
+        id="outlined-basic"
+        label="comment"
+        variant="outlined"
+        multiline
+        value={comment}
+        onChange={handleChange}
+        placeholder="Input"
+        type="reset"
+      />
+      <div>
+        <button onClick={() => console.log(comment)} type="submit">
+          submit
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Comment
+export default Comment;
