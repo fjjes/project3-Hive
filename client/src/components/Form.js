527976903a9f00e98769648c39cc1c38ce73e5fb
lucienlvmi <77748225+lucienlvmi@users.noreply.github.com>
2021-06-22 17:17:08 -0600

    added submit button

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Narrative from "./questiontypes/Narrative";
 import Comment from "./questiontypes/Comment";
-import SelectInput from "./questiontypes/SelectInput";
+//import SelectInput from "./questiontypes/SelectInput";
 import Slider from "./questiontypes/Slider";
 import RadioButtons from "./questiontypes/RadioButtons";
 import Checkboxes from "./questiontypes/Checkboxes";
+
 
 const Form = () => {
   return (
     <div>
       <Narrative />
       <Comment />
-      <SelectInput />
+     {/* //<SelectInput /> */}
       <Slider />
       <RadioButtons/>
       <Checkboxes />
