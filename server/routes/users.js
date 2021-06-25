9583355d33409c43a2fc5d3a74994247b80277e6
Christy <57055910+christyc01@users.noreply.github.com>
2021-06-25 16:19:47 -0600

    Get database connected, user model

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  let data = await User.find({});
+  console.info(`records retrieved from mongoose:`, 
+  data?.length);
+  res.send(data);
 });
 
 module.exports = router;
