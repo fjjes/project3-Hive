76e0ef19306b48a93bad47eaee65b87e92c57317
Christy <57055910+christyc01@users.noreply.github.com>
2021-06-28 15:03:25 -0600

    Adjust answer schema

--- server/models/Answer.js
+++ server/models/Answer.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const answerSchema = new Schema({
-personSurveyed:{ //access code, email, or name? Or all of them ? 
+department:{ //access code, email, or name? Or all of them ? 
     type:String,
     required:true,
-    unique:true
 },
 surveyNumber: Number,
 version: Number, //everyone in various levels in the organization get the same survey? are they different?
