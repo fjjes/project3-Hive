304f6dcf35b8a5c39fc1a09afe780fc661e80d6e
fjjes <fjjesreen@gmail.com>
2021-06-29 01:35:54 -0600

    added paper for surveypages

--- client/src/components/pages/SurveyQuestionPage.js
+++ client/src/components/pages/SurveyQuestionPage.js
@@ -1,9 +1,22 @@
 import React, {useState} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import SurveyQuestion from "../SurveyQuestion";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        paddingTop: 16,
+        paddingBottom: 16,
+        marginTop: theme.spacing.unit * 3,
+        paddingRight: 16,
+        paddingLeft: 16
+    },
+  }));
+
 const SurveyQuestionPage = () => {
+    const classes = useStyles();
     const [questionNumber, setQuestionNumber]= useState(1)
-    const [values, setValues]=useState()
+    const [values, setValues]=useState()//!!!!!!!!!!!!!!!!!!!!!!!
 
     const goToNextQuestion =()=>{
         let counter = questionNumber + 1
@@ -15,7 +28,8 @@ const SurveyQuestionPage = () => {
     }
 
     return (
-        <div className="survey-question-page">
+        <div>
+            <Paper className={classes.root} elevation={4}>
             <SurveyQuestion questionNumber={questionNumber} />
             {
                 questionNumber === 7 ? 
@@ -28,7 +42,8 @@ const SurveyQuestionPage = () => {
                         <button onClick={goToNextQuestion}>Next</button>
                     </div>
                 )
-            } 
+            }
+            </Paper> 
         </div>
     );
 }
