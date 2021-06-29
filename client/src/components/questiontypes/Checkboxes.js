951f783d86d84fd6e24593649d9edd56d17deb78
Christy <57055910+christyc01@users.noreply.github.com>
2021-06-28 20:09:18 -0600

    Remove unused code

--- client/src/components/questiontypes/Checkboxes.js
+++ client/src/components/questiontypes/Checkboxes.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import Checkbox from "@material-ui/core/Checkbox";
-import FormLabel from "@material-ui/core/FormLabel";
 import FormControl from "@material-ui/core/FormControl";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormHelperText from "@material-ui/core/FormHelperText";
 import { makeStyles } from "@material-ui/core/styles";
 import { TextField } from "@material-ui/core";
 
