277e3163b016a832ac82ff0dd0642eef31d894e6
Christy <57055910+christyc01@users.noreply.github.com>
2021-06-29 22:08:45 -0600

    Remove unused code

--- client/src/components/questiontypes/Checkboxes.js
+++ client/src/components/questiontypes/Checkboxes.js
@@ -1,29 +1,6 @@
 import React, { useState } from "react";
-// import "../Form.css"
-// import Checkbox from "@material-ui/core/Checkbox";
-// import FormControl from "@material-ui/core/FormControl";
-// import FormGroup from "@material-ui/core/FormGroup";
-// import FormControlLabel from "@material-ui/core/FormControlLabel";
-// import { makeStyles } from "@material-ui/core/styles";
-// import { TextField } from "@material-ui/core";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: "flex",
-//   },
-//   formLabel: {
-//     color: "#000",
-//     "&.Mui-focused": {
-//       color: "#000",
-//     },
-//   },
-//   // formControl: {
-//   //   margin: theme.spacing(3),
-//   // },
-// }));
 
 function Checkboxes({ questionNumber }) {
-  // const classes = useStyles();
   const [options, setOptions] = useState([
     { checked: false, value: "Option 1" },
     { checked: false, value: "Option 2" },
@@ -77,37 +54,24 @@ function Checkboxes({ questionNumber }) {
   return (
     <div className="question-component">
       <form className="checkbox-form-control">
-        {/* <FormControl
-        error={error}
-        component="fieldset"
-        className={classes.formControl}
-      > */}
         <p className="question-intro">
           Q{questionNumber}) {question}
         </p>
         <div className="checkbox-form-group">
-          {/* <input type="checkbox" value="test" />
-          <label>Test</label> */}
-          {/* <FormGroup> */}
           {options.map((option) => (
-            // <div className="checkbox-form-control-label">
             <div>
               <input
                 type="checkbox"
-                // <Checkbox
                 disabled={disabled && !option.checked}
                 checked={option.checked}
                 onChange={handleChange}
                 name="option"
                 id="option"
                 value={option.value}
-                // color='primary'
               />
               <label
                 for="option"
-                // <FormControlLabel
                 key={option.value}
-                // control={
               >
                 {option.value}
               </label>
@@ -115,24 +79,19 @@ function Checkboxes({ questionNumber }) {
           ))}
           <input
             type="text"
-            // <TextField
-            // fullWidth
             disabled={!options[options.length - 1].checked}
             label={`Add details for option ${options.length}.`}
             id="outlined-basic"
-            // variant="outlined"
             name="option"
             value={comment}
             onChange={(e) => setComment(e.target.value)}
           />
-          {/* </FormGroup> */}
         </div>
         <div>
           <button onClick={handleSubmit} type="submit">
             Submit
           </button>
         </div>
-        {/* </FormControl> */}
       </form>
     </div>
   );
