f0027cc9a42dff92238b29aa9f6c1d71462763df
Nikita George <nikkigeorge2@gmail.com>
2021-06-29 21:18:33 -0600

    Changes to slider

--- client/src/components/pages/SurveyQuestionPage.js
+++ client/src/components/pages/SurveyQuestionPage.js
@@ -1,51 +1,50 @@
-import React, {useState} from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 import SurveyQuestion from "../SurveyQuestion";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        paddingTop: 16,
-        paddingBottom: 16,
-        marginTop: theme.spacing.unit * 3,
-        paddingRight: 16,
-        paddingLeft: 16
-    },
-  }));
+  root: {
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+    paddingRight: 16,
+    paddingLeft: 16,
+  },
+}));
 
 const SurveyQuestionPage = () => {
-    const classes = useStyles();
-    const [questionNumber, setQuestionNumber]= useState(1)
-    const [values, setValues]=useState()//!!!!!!!!!!!!!!!!!!!!!!!
+  const classes = useStyles();
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [values, setValues] = useState(); //!!!!!!!!!!!!!!!!!!!!!!!
 
-    const goToNextQuestion =()=>{
-        let counter = questionNumber + 1
-        setQuestionNumber(counter)
-    }
+  const goToNextQuestion = () => {
+    let counter = questionNumber + 1;
+    setQuestionNumber(counter);
+  };
 
-    const handleSubmit = () => {
-        console.log(values)
-    }
+  const handleSubmit = () => {
+    console.log(values);
+  };
 
-    return (
-        <div>
-            <Paper className={classes.root} elevation={4}>
-            <SurveyQuestion questionNumber={questionNumber} />
-            {
-                questionNumber === 7 ? 
-                (
-                    <div>
-                        <button onClick={handleSubmit} type="submit" >Submit</button>
-                    </div>
-                ):(
-                    <div>
-                        <button onClick={goToNextQuestion}>Next</button>
-                    </div>
-                )
-            }
-            </Paper> 
-        </div>
-    );
-}
- 
+  return (
+    <div>
+      <Paper className={classes.root} elevation={4}>
+        <SurveyQuestion questionNumber={questionNumber} />
+        {questionNumber === 7 ? (
+          <div>
+            <button onClick={handleSubmit} type="submit">
+              Submit
+            </button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={goToNextQuestion}>Next</button>
+          </div>
+        )}
+      </Paper>
+    </div>
+  );
+};
+
 export default SurveyQuestionPage;
