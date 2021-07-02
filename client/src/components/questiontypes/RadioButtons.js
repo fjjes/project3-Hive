2849c35c6308ddebccf085e9fc7cbea9329909c3
fjjes <fjjesreen@gmail.com>
2021-07-01 18:37:59 -0600

    RadioButton fixed

--- client/src/components/questiontypes/RadioButtons.js
+++ client/src/components/questiontypes/RadioButtons.js
@@ -5,18 +5,28 @@ import React, { useState } from 'react';
 // import FormControl from '@material-ui/core/FormControl';
 
  function RadioButtons({questionNumber}) {
-  const [value, setValue] = React.useState('');
-  const [options, setOptions] = useState([
-    {value: "option1", label: "Option 1"},
-    {value: "option2", label: "Option 2"},
-    {value: "option3", label: "Option 3"},
-    {value: "option4", label: "Option 4"},
-    {value: "option5", label: "Option 5"},
-    {value: "option6", label: "Option 6"},
-    {value: "option7", label: "Option 7"},
-    {value: "option8", label: "Option 8"}
-    
-  ])
+  const [value, setValue] = useState('');
+  // const [options, setOptions] = useState([
+  //   {value: "option1", label: "Option 1"},
+  //   {value: "option2", label: "Option 2"},
+  //   {value: "option3", label: "Option 3"},
+  //   {value: "option4", label: "Option 4"},
+  //   {value: "option5", label: "Option 5"},
+  //   {value: "option6", label: "Option 6"},
+  //   {value: "option7", label: "Option 7"},
+  //   {value: "option8", label: "Option 8"} 
+  // ])
+
+  let options = [
+    'Option1',
+    'Option2',
+    'Option3',
+    'Option4',
+    'Option5',
+    'Option6',
+    'Option7',
+    'Option8'
+  ]
 
   const handleChange = (e) => {
     setValue(e.target.value);
@@ -28,36 +38,24 @@ import React, { useState } from 'react';
   const question = "What is your department or team?"
 
   return (
-    <div className="question-component">
-      <form >    
+    <div className="question-component">   
         <p className="question-intro">Q{questionNumber} {question}</p>
-        <div className ="RadioGroup" 
-        aria-label="radio" 
-        name="radio1" 
-        value={value} 
-        onChange={handleChange}
-        >        
-          {options.map((option,index) => (
-           <div key={index}> 
-           <label htmlFor ="radio"
-              key={option.value}>
-              {option.label}
-              </label>
+          {options.map((option,index) => {
+           return(<div key={index}> 
               <input type="radio"
                   id="radio"
                   onChange={handleChange}
-                  name="option"
-                  value={option.value}
+                  name="option-group"
+                  value={option}
                   color='primary'/>
-                  </div>
-              ))}
+                  <label htmlFor ="radio">
+              {option}
+              </label>
+                  </div>)
+             })}
           <div>
-            <button onClick={handleSubmit} type="submit">
-              Submit
-            </button>
+            <button onClick={handleSubmit} type="submit">Submit</button>
           </div>
-        </div>
-        </form>
     </div>
   );
 }
