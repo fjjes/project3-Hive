3c0f3d1d5b79198319b56c98e15f8932478a58e9
Christy <57055910+christyc01@users.noreply.github.com>
2021-07-05 10:25:23 -0600

    Fix "class" and "for" warnings

--- client/src/components/pages/Landing Page/Start.js
+++ client/src/components/pages/Landing Page/Start.js
@@ -7,26 +7,26 @@ export default function Start() {
     <div className="start">
       <h1>Welcome!</h1>
       <h2>Enter Your Access Code Here:</h2>
-      <input class="c-checkbox" type="checkbox" id="checkbox"></input>
-      <div class="c-formContainer">
-        <form class="c-form" action="">
+      <input className="c-checkbox" type="checkbox" id="checkbox"></input>
+      <div className="c-formContainer">
+        <form className="c-form" action="">
           <input
-            class="c-form__input"
+            className="c-form__input"
             placeholder="Code"
             type="email"
             pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
             required
           ></input>
-          <label class="c-form__buttonLabel" for="checkbox">
+          <label className="c-form__buttonLabel" htmlFor="checkbox">
             <Link to="/survey">
-              <button class="c-form__button" type="button">
+              <button className="c-form__button" type="button">
                 Enter
               </button>
             </Link>
           </label>
           <label
-            class="c-form__toggle"
-            for="checkbox"
+            className="c-form__toggle"
+            htmlFor="checkbox"
             data-title="Enter Code"
           ></label>
         </form>
