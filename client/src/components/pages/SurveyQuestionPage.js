fc22e122847a437d0667cf8aca45806abbaaf721
lucienlvmi <77748225+lucienlvmi@users.noreply.github.com>
2021-07-07 11:14:50 -0600

    moved progress bar at the end

--- client/src/components/pages/SurveyQuestionPage.js
+++ client/src/components/pages/SurveyQuestionPage.js
@@ -57,7 +57,7 @@ const SurveyQuestionPage = () => {
 
   return (
     <div className='survey-page'>
-      <Progress done={progressBarDone}/>
+      
       <Paper className={classes.root} elevation={4}>
         {/* <SurveyQuestion questionNumber={questionNumber} /> */}
         <SurveyQuestion questionBlock={questionArray[questionNumber]}/>
@@ -77,6 +77,7 @@ const SurveyQuestionPage = () => {
           </div>
         ))}
       </Paper>
+      <Progress done={progressBarDone}/>
     </div>
   );
 };
