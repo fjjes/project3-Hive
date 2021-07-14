87c7f3f5d02fcba24dddc3642fb33b2640fbd823
lucienlvmi <77748225+lucienlvmi@users.noreply.github.com>
2021-07-14 17:22:42 -0600

    adding i to make it not case sensitive - CA Regex

--- client/src/components/questiontypes/PostalCode.js
+++ client/src/components/questiontypes/PostalCode.js
@@ -1,15 +1,14 @@
-import React, { useEffect,useState, useContext } from 'react';
+import React, {useState, useContext } from 'react';
 import { AnswerContext } from '../pages/SurveyQuestionPage';
 
 const PostalCode =({questionNumber, question})=> {
   const {answers, setAnswers} = useContext(AnswerContext)
-//   const [postalcode, setPostalCode] = useState('');
+  const [postalcode, setPostalCode] = useState('');
   const [error, setError] =useState('');
 
   const handleChange = (e)=>{
-    //   setPostalCode((e.target.value))
-        if (/^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$/.test(e.target.value) || /^[0-9]{5}(?:-[0-9]{4})?$/.test(e.target.value) ) {
-
+      setPostalCode((e.target.value))
+        if (/^[abceghjklmnprstvxy][0-9][abceghjklmnprstvwxyz]\s?[0-9][abceghjklmnprstvwxyz][0-9]$/i.test(e.target.value) || /^[0-9]{5}(?:-[0-9]{4})?$/.test(e.target.value) ) {
             let updateAnswers = {...answers}
             updateAnswers[questionNumber]=e.target.value
             setAnswers(updateAnswers) 
@@ -19,9 +18,9 @@ const PostalCode =({questionNumber, question})=> {
         }
     
   }
-//  const isZipValidUs = postalCodeus.length === 5 && postalcode;
-//  const isZipValidCanada = postalCodecanada.length === 6 && postalcode;
-
+//    /^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$/ - CA postal code - case sensitive
+// /^[abceghjklmnprstvxy][0-9][abceghjklmnprstvwxyz]\s?[0-9][abceghjklmnprstvwxyz][0-9]$/i - CA postal code adding i at the end makes it insensitive
+//   /^[0-9]{5}(?:-[0-9]{4})?$/ USA postal code 
 //     function validateCityOrPostalCode(postalcode) {
 //     return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(postalcode);
 //   }
@@ -34,15 +33,13 @@ const PostalCode =({questionNumber, question})=> {
       <p className="question-intro">Q{questionNumber}.</p><span>
       <p className="question-intro">{question}</p></span>
       <input
-        rows="4"
-        cols="50"
+        // rows="4"
+        // cols="50"
         label="postalcode"
         // value={postalcode}
         value={answers[questionNumber]}
         onChange={handleChange}
         option="Postal Code"
-        // setAnswer(value.replace(^[0-9]{5}(?:-[0-9]{4})?$, "").substr(0, 5));
-        // setAnswer(value.replace(^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$, "").substr(0, 6));
         
       />
       <p>{error}</p>
