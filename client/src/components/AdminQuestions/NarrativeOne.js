513b3953fac19f848bf430c4978133a29269bc88
Christy <57055910+christyc01@users.noreply.github.com>
2021-07-14 23:11:26 -0600

    Make submit button console.log narrative input value and also a list of selected component names

--- client/src/components/AdminQuestions/NarrativeOne.js
+++ client/src/components/AdminQuestions/NarrativeOne.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-function NarrativeOne() {
+function NarrativeOne(props) {
+
+  function handleNarrativeChange(event) {
+    props.onChange(event.target.value)
+  }
+
   return (
     <div className="question-component admin-question-component narrative-component">
-      <input type="text" placeholder="Insert narrative text here..." />
+      <input type="text" placeholder="Insert narrative text here..." value={props.value} onChange={handleNarrativeChange}/>
     </div>
   );
 }
