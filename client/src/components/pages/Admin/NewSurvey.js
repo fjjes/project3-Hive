367edb8511cd6da837474cdd48e3b665824dda92
Christy <57055910+christyc01@users.noreply.github.com>
2021-07-16 17:54:41 -0600

    Add the ability to post survey name, number, version, narrative, and component list to the DB

--- client/src/components/pages/Admin/NewSurvey.js
+++ client/src/components/pages/Admin/NewSurvey.js
@@ -12,16 +12,18 @@ import SliderTwo from "../../AdminQuestions/SliderTwo"; // Not working
 import { v4 as uuidv4 } from "uuid";
 
 const NewSurvey = (props) => {
-  const [companyName, setCompanyName] = useState("");
-  const [surveyName, setSurveyName] = useState("");
+  const [company, setCompany] = useState("");
+  const [version, setVersion] = useState("");
   const [componentList, setComponentList] = useState([]);
   const [narrativeTextValue, setNarrativeTextValue] = useState("");
 
-  const url = `localhost:4444/${uuidv4()}`;
+  // Create uuid to be used as survey number
+  const uuid = uuidv4();
+  const url = `localhost:4444/${uuid}`;
 
+  // Add question components (from /questiontypes folder) to the custom survey, depending on the id of the clicked button (buttons are in the return statement)
   function addComponent(e) {
     // console.log('Clicked:', e.target.id);
-
     let InsertedComponent;
     switch (e.target.id) {
       case "narrative":
@@ -54,7 +56,8 @@ const NewSurvey = (props) => {
       default:
     }
 
-    // Will need to update handleNarrativeChange in the onChange below to be more generic
+    // Will need to update handleNarrativeChange in the onChange below to be generic, so it works for the other question components
+    // This creates an array of the components that have been added to the custom survey
     setComponentList(
       componentList.concat(
         <InsertedComponent
@@ -73,28 +76,59 @@ const NewSurvey = (props) => {
     setNarrativeTextValue(newValue);
   }
 
+  function onInputChange(event, setFunction) {
+    setFunction(event.target.value);
+  }
+
   async function handleSubmit() {
+    const surveyNumber = uuid;
 
-    console.log("***** DATA TO SAVE TO DATABASE *****");
-    console.log("companyName: ", companyName);
-    console.log("surveyName: ", surveyName);
-    console.log("narrativeTextValue: ", narrativeTextValue);
-
-    // This just creates a list of the names of the components that have been added to the custom survey
+    // This just creates a list of the names of the components that have been added to the custom survey - to be updated to include more than just the names...
     let componentListNames = [];
     for (let i = 0; i < componentList.length; i++) {
       componentListNames = [...componentListNames, componentList[i].type.name];
     }
     console.log("List of selected components: ", componentListNames);
-  }
+    let componentListNamesString = componentListNames.toString();
+    console.log("componentListNamesString: ", componentListNamesString);
+    
+    // Log the info being saved into the DB
+    console.log("***** DATA TO SAVE TO DATABASE *****");
+    console.log("surveyNumber: ", surveyNumber)
+    console.log("company: ", company);
+    console.log("version: ", version);
+    console.log("narrativeTextValue: ", narrativeTextValue);
+    console.log("componentListNamesString: ", componentListNamesString)
 
-  function onInputChange(event, setFunction) {
-    setFunction(event.target.value);
+    // "questions" (below) will need to be edited so it saves more than just the question names into the database
+    let currentDate = new Date();
+    let surveyToCreate = {
+      surveyNumber,
+      company,
+      version,
+      narrative: narrativeTextValue,
+      createdDate: currentDate,
+      questions: {
+        questionType: componentListNamesString,
+      },
+    };
+
+    // Post the custom survey data to the DB
+    let createSurvey = await fetch("/api/survey", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(surveyToCreate),
+    });
+    console.log("Creating a custom-built survey, yay!", surveyToCreate);
+
+    if (createSurvey.status === 200) {
+      console.log("create response is successful");
+    }
   }
 
   return (
     <div>
-{/* TOP PART OF PAGE */}
+      {/* TOP PART OF PAGE */}
       <h2>
         Build your own survey by choosing from the components on the left.
       </h2>
@@ -104,18 +138,18 @@ const NewSurvey = (props) => {
           id="company-name"
           className="survey-info"
           placeholder="Company name"
-          onChange={(event) => onInputChange(event, setCompanyName)}
+          onChange={(event) => onInputChange(event, setCompany)}
         />
         <input
           id="survey-name"
           className="survey-info"
           placeholder="Survey name/version"
-          onChange={(event) => onInputChange(event, setSurveyName)}
+          onChange={(event) => onInputChange(event, setVersion)}
         />
       </div>
 
-{/* LEFT PART OF PAGE */}
-{/* FOR LATER:  Make it so we can change order and number of questions - drag and drop?? */}
+      {/* LEFT PART OF PAGE */}
+      {/* FOR LATER:  Make it so we can change order and number of questions - drag and drop?? */}
       <div className="survey-selection-container">
         <div className="survey-selection-sidebar">
           <button id="narrative" onClick={addComponent}>
@@ -147,15 +181,16 @@ const NewSurvey = (props) => {
           </button>
         </div>
 
-{/* RIGHT PART OF PAGE */}
+        {/* RIGHT PART OF PAGE */}
         <div className="survey-selected-components">
           <div className="survey-selected-components-background">
+            {/* Displays the question components that have been selected */}
             <div>{componentList}</div>
           </div>
         </div>
       </div>
 
-{/* BOTTOM PART OF PAGE */}
+      {/* BOTTOM PART OF PAGE */}
       <div className="dividerLine"></div>
       <div className="save-survey-button-and-link">
         <button
