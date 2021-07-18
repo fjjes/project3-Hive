346d95926a1882ddb548f25c1587654d420bd342
fjjes <fjjesreen@gmail.com>
2021-07-17 21:48:53 -0600

    Navbar to admin

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../../images/hivetagline2.png'
+import {Nav, NavLink, Bars, NavMenu, NavBtnLink} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        {/* <NavLink to='/'>
+          <img src={logo} alt="logo"  style={{ width:'50%', alignItems:'flex-start'}}/>
+        </NavLink> */}
+        <Bars />
+        <NavMenu>
+          {/* <NavLink to='/' activeStyle>Home</NavLink> */}
+          <NavLink to='/create-new' activeStyle>Create New</NavLink>
+          <NavLink to='/find-list' activeStyle>Find Surveys</NavLink>
+          <NavLink to='/data-vis' activeStyle>Data Vis</NavLink>
+          <NavBtnLink to='/sign-out'>Sign out</NavBtnLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ client/src/components/Navbar/NavbarElements.js
@@ -0,0 +1,81 @@
+import { NavLink as Link } from 'react-router-dom'
+import {FaBars} from 'react-icons/fa'
+import styled from 'styled-components'
+
+
+export const Nav = styled.nav`
+  background: #393F4B;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1000px) / 2);
+  z-index: 10;
+`;
+
+export const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  
+  &.active {
+    color: #f59645;
+  }
+`;
+
+export const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+  margin-right: 24px; 
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtnLink = styled(Link)`
+  border-radius: 4px;
+  background: #2DD9BC;
+  padding: 10px 22px;
+  color: #393F4B;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  margin-left: 24px;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #f59645;
+    color: #010606;
+  }
+`;
+
+const active = "#f59645" // Orange
+  const inactive = "#bce6f8" // Dark blue
