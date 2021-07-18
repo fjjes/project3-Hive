05c83906c9d848eb8d9be90f6673f0984c014898
fjjes <fjjesreen@gmail.com>
2021-07-18 11:07:11 -0600

    landing page to the link

--- client/src/components/pages/LandingPage/Logo.js
+++ client/src/components/pages/LandingPage/Logo.js
@@ -1,27 +1,39 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { Player } from "@lottiefiles/react-lottie-player";
-import "../LandingPage/Logo.css";
-import { Link } from "react-router-dom";
+import SurveyQuestionPage from "../SurveyQuestionPage";
+import "./Logo.css";
+//import { Link } from "react-router-dom";
 import image from "../../../images/website.jpg"
 
-export default function Logo({ flashcard }) {
+export default function Logo({ flashcard}) {
+  const {surveyId}= useParams()
+  const [survey, setSurvey]=useState()
   const [narrative, setNarrative] = useState("");
   const [company, setCompany] = useState("");
+  const [questionArray, setQuestionArray] = useState([]);
   const [flip, setFlip] = useState(false);
+  const [showQuestions, setShowQuestions]=useState(false)
 
   useEffect(() => {
     const getSurveyQuestions = async () => {
-      let response = await fetch("/api/survey"); //should be get by id
+      let response = await fetch(`/api/survey/${surveyId}`); 
       let data = await response.json();
       console.log("retrieved data:", data);
-      console.log("narrative:", data[0].narrative);
-      setNarrative(data[0].narrative);
-      setCompany(data[0].company);
+      setSurvey(data)
+      console.log("narrative:", data.narrative);
+      setNarrative(data.narrative);
+      setCompany(data.company);
+      setQuestionArray(data.questions)
+      console.log('Survey questions:', data.questions)
     };
     getSurveyQuestions();
-  }, []);
+  }, [surveyId]);
 
+  
   return (
+    <div>
+      {showQuestions=== false ? (
     <div
       className={`card ${flip ? "flip" : ""}`}
       onClick={() => setFlip(!flip)}
@@ -38,10 +50,10 @@ export default function Logo({ flashcard }) {
       <div className="theback"
       >
         <h1>Hello {company} Team!</h1>
-        <img src={image} style={{height:100}}/>
+        <img src={image} style={{height:100}} alt="start-img"/>
         <p>{narrative}</p>
        
-        <Link to="/survey">
+        {/* <Link to="/survey">
           <button className="logo-button" type="button">
             ENTER
           </button>
@@ -50,14 +62,23 @@ export default function Logo({ flashcard }) {
           <button className="neu-button" type="button">
             ENTER
           </button>
-        </Link>
-        <div className="adminportal"
+        </Link> */}
+        {/* <div className="adminportal"
           >
         <Link to="/admin">
         <button className="admin-portal-button">Admin Portal</button>
         </Link>
-          </div>
+          </div> */}
+        <button className="logo-button" type="button" onClick={()=>setShowQuestions(true)}>ENTER</button>
+        <button className="neu-button" type="button" onClick={()=>setShowQuestions(true)}>ENTER</button>
       </div>
     </div>
+      ):(
+      <div>
+        <SurveyQuestionPage survey={survey} questionArray={questionArray}/>
+      </div>
+      )}
+      </div>
+
   );
 }
