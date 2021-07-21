28ef63ab9c66a4f1556d1cd32ab1545b2e596596
Christy <57055910+christyc01@users.noreply.github.com>
2021-07-21 14:41:13 -0600

    Trying to get the old narrative pulled into the edit function and the updated narrative to save

--- client/src/components/AdminQuestions/NarrativeOne.js
+++ client/src/components/AdminQuestions/NarrativeOne.js
@@ -1,74 +1,101 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { useState, useEffect } from "react";
 import * as BsIcons from "react-icons/bs";
-import * as RiIcons from "react-icons/ri";
 import * as GiIcons from "react-icons/gi";
 import * as MdIcons from "react-icons/md";
 
-function NarrativeOne(props) {
-  const [inEditMode, setInEditMode] = useState({ status: false, rowKey: null });
+function NarrativeOne({passStuff}) {
+  const [inEditMode, setInEditMode] = useState({ status: false });
+  const [narrative, setNarrative] = useState(
+    "Insert narrative text here... Lorem ipsum dolor sit amet consectetur, adipisicing elit. In autem laborum natus dicta amet quis quaerat a nobis aspernatur, ex commodi accusantium, assumenda sed optio possimus impedit quam quidem exercitationem neque cum corrupti maiores id."
+  );
 
-  const narrativeTextDefault =
-    "Insert narrative text here... Lorem ipsum dolor sit amet consectetur, adipisicing elit. In autem laborum natus dicta amet quis quaerat a nobis aspernatur, ex commodi accusantium, assumenda sed optio possimus impedit quam quidem exercitationem neque cum corrupti maiores id. Modi, cum, similique temporibus dignissimos vero odit fuga, repellat obcaecati doloribus ullam cupiditate quaerat libero! Ea cum libero, nostrum iure ipsam praesentium itaque consequuntur nisi ipsum assumenda id enim magni reprehenderit fuga iste aliquam quas. Error libero consequuntur alias laboriosam.";
-
-  const onEditClicked = (id, currentNarrative) => {
-    setInEditMode({ status: true, key: id });
-    // setNarrative(currentNarrative)
-    console.log("clicked edit");
-  };
-
-  const handleDeleteClick = () => {
-    console.log("clicked delete");
-  };
-
-  function handleNarrativeChange(event) {
-    props.onChange(event.target.value);
+  const handleNarrativeChange = (e) => {
+    console.log('narrative edit...')
+    setNarrative(e.target.value)
   }
+
+  const onEditClicked = () => {
+    console.log("clicked edit");
+    setInEditMode({ status: true });
+  };
+  
+  const onSave = () => {
+    console.log("clicked save");
+    setNarrative(narrative)
+    setInEditMode({ status: false });
+    passStuff(narrative)
+  };
+
+  const onCancel=()=>{
+    console.log("clicked cancel")
+    setInEditMode({status:false })
+  }
+  
+  console.log("The narrative that works! (from NarrativeOne): ", narrative)  // This one is correct!!!
+
+  useEffect(()=>{
+    console.log("Narrative from useEffect: ", narrative)
+ },[narrative])
 
   return (
     <div className="question-component admin-question-component narrative-component">
-      <h2 style={{textAlign: "left"}}>Narrative</h2>
+      <h2 style={{ textAlign: "left" }}>Narrative</h2>
       <div className="narrative">
         <div className="narrative-text-area">
           {inEditMode.status ? (
             <textarea
+              className="narrative-text-area-input"
               rows="10"
-              cols="100"
               type="text"
-              // value={props.value}
-              defaultValue={narrativeTextDefault}
+              value={narrative}
               onChange={handleNarrativeChange}
-            >
-            </textarea>
+            />
           ) : (
-            <p>{narrativeTextDefault}</p>
+            <p>{narrative}</p>
           )}
         </div>
         <div className="narrative-buttons">
-          <button
-            className="clear icn3"
-            title="Edit"
-            onClick={() =>
-              onEditClicked()
-              // narrative
-            }
-          >
-            <BsIcons.BsPencilSquare />
-          </button>
-          <span className="slash" style={{ color: "#fff" }}>
-            /
-          </span>
-          <button
-            className="clear icn4"
-            title="Delete"
-            onClick={() => {
-              handleDeleteClick();
-              // row._id
-            }}
-          >
-            <RiIcons.RiDeleteBinFill />
-          </button>
+          {inEditMode.status ? (
+            <div>
+              <button
+                className="clear icn1"
+                title="Save"
+                onClick={() =>
+                  onSave()
+                  // row._id, company, version, surveyNum
+                }
+              >
+                <GiIcons.GiSaveArrow />
+              </button>
+              <span className="slash" style={{ color: "#fff" }}>
+                /
+              </span>
+              <button
+                className="clear icn2"
+                title="Cancel"
+                onClick={() => onCancel()}
+                >
+                <MdIcons.MdCancel />
+              </button>
+                </div>
+              ) : (
+            <div>
+              <button
+                className="clear icn3"
+                title="Edit"
+                onClick={
+                  () => onEditClicked()
+                  // narrative
+                }
+              >
+                <BsIcons.BsPencilSquare />
+              </button>
+              <span className="slash" style={{ color: "#fff" }}>
+                /
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
