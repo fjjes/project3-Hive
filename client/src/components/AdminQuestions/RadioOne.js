f2a02e05b4207ad10d70c23bb568a37f48d61eb4
lucienlvmi <77748225+lucienlvmi@users.noreply.github.com>
2021-07-27 16:14:21 -0600

    radio button working

--- client/src/components/AdminQuestions/RadioOne.js
+++ client/src/components/AdminQuestions/RadioOne.js
@@ -9,10 +9,9 @@ import "../pages/Admin/AdminPortal.css";
 function RadioOne({ questionNumber }) {
   const { questions, setQuestions } = useContext(QuestionContext);
   const [inEditMode, setInEditMode] = useState({ status: false });
+  const [radioOption, setRadioOption] = useState("");
   const [question, setQuestion] = useState("What is your department or team?");
   const [answerOptions, setAnswerOptions] = useState([
-    <input 
-     placeholder = "answerOptions"></input>
     // "Option1",
     // "Option2",
     // "Option3",
@@ -29,8 +28,11 @@ function RadioOne({ questionNumber }) {
   };
 
   const onSave = () => {
-    setQuestion(question);
-    console.log("clicked save"); 
+    setQuestions(questions);
+  //   const previousQuestions=questions
+  //   previousQuestions[questionNumber]={}
+  //   setQuestion(previousQuestions)
+  //   console.log("clicked save", questions);
     setInEditMode({ status: false });
   };
   const onCancel = () => {
@@ -40,120 +42,123 @@ function RadioOne({ questionNumber }) {
 
   const OnAddInput = () => {
     console.log("clicked add");
-    setAnswerOptions ([...answerOptions, <input placeholder = "value"/>])
-    console.log ("answer", answerOptions)
+    setAnswerOptions([...answerOptions, radioOption]);
+    console.log("answer", answerOptions);
     setInEditMode({ status: true });
   };
-
-useEffect(() => {
-  const newQuestionList = [...questions];
-  newQuestionList[questionNumber - 1] = {
-    ...newQuestionList[questionNumber - 1],
-    question,
-    questionNumber,
-    answerOptions,
+  const onInputChange = (event, index) => {
+    const previousAnswerOptions = answerOptions;
+    previousAnswerOptions[index] = event.target.value;
+    setAnswerOptions(previousAnswerOptions);
   };
-  setQuestions(newQuestionList);
-}, []);
 
-return (
-  <div className="radio-one question-component admin-question-component">
-    <p className="question-intro">Q{questionNumber}.</p>
-    {inEditMode.status ? (
-            <input
-            type="text"
-            value = {question} 
-            questionNumber={questionNumber}
-            onChange={(e)=>setQuestion (e.target.value)}
-            />
-            ) : (
-            <p className="question-intro">{question}</p>
-            )}
-    <div className="questionAndButtons">
-    <div className="radiobutton">
-      {answerOptions.map((option, index) => {
-         return (
-           inEditMode.status ? (
-         
-            <div key={index}>
-              <input
-                type="radio"
-                id={option}
-                name="option-group"
-                value={option}
-                color="primary"
-                questionNumber={questionNumber}
-                onChange={(e) => setAnswerOptions([e.target.value])}
-              />
-              <label htmlFor={option}>{option}</label>
-            </div>
-          ):  
-      ( 
-        <div>
-        <input
-                type="radio"
-                id={option}
-                name="option-group"
-                value={option}
-                color="primary"
-                questionNumber={questionNumber}
-                // onChange={(e) => setQuestion(e.target.value)}
-              />
-        <label htmlFor={option}>{option}</label>
-        </div>
-      )
-      // <input />
-    )}
-    )}
-    <div className="radio-buttons">
+  useEffect(() => {
+    const newQuestionList = [...questions];
+    newQuestionList[questionNumber - 1] = {
+      ...newQuestionList[questionNumber - 1],
+      question,
+      questionNumber,
+      answerOptions,
+    };
+    setQuestions(newQuestionList);
+  }, [radioOption]);
+
+  return (
+    <div className="radio-one question-component admin-question-component">
+      <p className="question-intro">Q{questionNumber}.</p>
       {inEditMode.status ? (
-        <div className="edit-button">
-          <button className="clear icn1" title="Save" onClick={() => onSave()}>
-            <GiIcons.GiSaveArrow />
-          </button>
-          <span className="slash" style={{ color: "#fff" }}>
-            /
-          </span>
-          <button
-            className="clear icn2"
-            title="Cancel"
-            onClick={() => onCancel()}
-          >
-            <MdIcons.MdCancel />
-          </button>
-        
-        
-        <div className="edit-button">
-          <button
-            className="clear icn4"
-            title="Add"
-            onClick={() => OnAddInput()}
-          >
-            < BsIcons.BsFillPlusCircleFill/>
-          </button>
-        </div>
-        </div> 
-        // </div>
-      ) : ( 
-       
-      <div className="edit-button">
-        <button
-          className="clear icn3"
-          title="Edit"
-          onClick={() => onEditClicked()}
-        >
-          <BsIcons.BsPencilSquare />
-        </button>
-        <span className="slash" style={{ color: "#fff" }}>
-          /
-        </span>
-      </div>
+        <input
+          type="text"
+          value={question}
+          questionNumber={questionNumber}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+      ) : (
+        <p className="question-intro">{question}</p>
       )}
+      <div className="questionAndButtons">
+        <div className="radioOne">
+          {answerOptions.map((option, index) => {
+            return inEditMode.status ? (
+              <div key={index}>
+                <input
+                  type="radio"
+                  id={option}
+                  name="option-group"
+                  // value={option}
+                  color="primary"
+                  questionNumber={questionNumber}
+                />
+                <input defaultValue={option} onChange={(e) => onInputChange(e, index)} />
+              </div>
+            ) : (
+              <div>
+                <input
+                  type="radio"
+                  id={option}
+                  name="option-group"
+                  // value={option}
+                  color="primary"
+                  questionNumber={questionNumber}
+                  // onChange={(e) => setQuestion(e.target.value)}
+                />
+                <label>{option}</label>
+              </div>
+            );
+            // <input />
+          })}
+          <div className="radio-buttons">
+            {inEditMode.status ? (
+              <div className="edit-button">
+                <button
+                  className="clear icn1"
+                  title="Save"
+                  onClick={() => onSave()}
+                >
+                  <GiIcons.GiSaveArrow />
+                </button>
+                <span className="slash" style={{ color: "#fff" }}>
+                  /
+                </span>
+                <button
+                  className="clear icn2"
+                  title="Cancel"
+                  onClick={() => onCancel()}
+                >
+                  <MdIcons.MdCancel />
+                </button>
+
+                <div className="edit-button">
+                  <button
+                    className="clear icn4"
+                    title="Add"
+                    onClick={() => OnAddInput()}
+                  >
+                    <BsIcons.BsFillPlusCircleFill />
+                  </button>
+                </div>
+              </div>
+            ) : (
+              // </div>
+              <div className="edit-button">
+                <button
+                  className="clear icn3"
+                  title="Edit"
+                  onClick={() => onEditClicked()}
+                >
+                  <BsIcons.BsPencilSquare />
+                </button>
+                <span className="slash" style={{ color: "#fff" }}>
+                  /
+                </span>
+              </div>
+            )}
+          </div>
+          {/* )};  */}
+        </div>
+      </div>
     </div>
-   {/* )};  */}
-</div>
-  </div>
-  </div>
-)}
+  );
+}
 
 export default RadioOne;
