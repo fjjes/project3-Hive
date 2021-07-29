c1323ab2b37dc04a6b44fcd2356eac8701af8426
fjjes <fjjesreen@gmail.com>
2021-07-29 10:32:10 -0600

    cleaned up findsurvey

--- client/src/components/pages/Admin/FindSurvey.js
+++ client/src/components/pages/Admin/FindSurvey.js
@@ -9,10 +9,6 @@ const FindSurvey = (props) => {
   const [rows, setRows] = useState([]);
   const [searchInputCompany, setSearchInputCompany] = useState("");
   const [searchInputNumber, setSearchInputNumber] = useState("");
-  // const [company, setCompany] = useState("");
-  // const [version, setVersion] = useState("");
-  // const [surveyNum, setSurveyNum] = useState();
-  // const [inEditMode, setInEditMode] = useState({ status: false, rowKey: null });
 
   props.resetRowId();
 
@@ -33,42 +29,7 @@ const FindSurvey = (props) => {
   useEffect(() => {
     getSurveyList();
   }, []);
-
-  // const updateSurveys = (id, newCompany, newVersion, newSurveyNumber) => {
-  //   let surveyToUpdate = {
-  //     company: newCompany,
-  //     version: newVersion,
-  //     surveyNumber: newSurveyNumber,
-  //   };
-
-  //   let updateResponse = fetch(`/api/survey/${id}`, {
-  //     method: "PUT",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify(surveyToUpdate),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((json) => {
-  //       // onCancel();
-  //       getSurveyList();
-  //       console.log("updateResponse:", updateResponse);
-  //     });
-  // };
-
-  // const onEditClicked = (id, currentCompany, currentVersion, currentSurveyNum) => {
-  //   setInEditMode({ status: true, rowKey: id });
-  //   setCompany(currentCompany);
-  //   setVersion(currentVersion);
-  //   setSurveyNum(currentSurveyNum);
-  // };
-
-  // const onSave = (id, newCompany, newVersion, newSurveyNumber) => {
-  //   updateSurveys(id, newCompany, newVersion, newSurveyNumber);
-  // };
-
-  // const onCancel = () => {
-  //   setInEditMode({ status: false, rowKey: null });
-  // };
-
+  
   const handleDeleteClick = async (id) => {
     let answer = window.confirm(`Confirm Deleting the survey?`);
     if (answer) {
@@ -114,75 +75,33 @@ const FindSurvey = (props) => {
           {rows.map((row, index) => {
             return (
               <tr key={index}>
-                <td>{row.company}
-                  {/* {inEditMode.status && inEditMode.rowKey === row._id ? (
-                    <input
-                      value={company}
-                      onChange={(e) => setCompany(e.target.value)}
-                    />
-                  ) : (
-                    row.company
-                  )} */}
-                </td>
-                <td>{row.version}
-                  {/* {inEditMode.status && inEditMode.rowKey === row._id ? (
-                    <input
-                      value={version}
-                      onChange={(e) => setVersion(e.target.value)}
-                    />
-                  ) : (
-                    row.version
-                  )} */}
-                </td>
-                <td>{row.surveyNumber}
-                  {/* {inEditMode.status && inEditMode.rowKey === row._id ? (
-                    <input
-                      value={surveyNum}
-                      onChange={(e) => setSurveyNum(e.target.value)}
-                    />
-                  ) : (
-                    row.surveyNumber
-                  )} */}
-                </td>
+                <td>{row.company}</td>
+                <td>{row.version}</td>
+                <td>{row.surveyNumber}</td>
                 <td>
                   <Link to={`/survey/${row._id}`}>{`http://localhost:4444/survey/${row._id}`}</Link>
                 </td>
                 <td>
-                  {/* {inEditMode.status && inEditMode.rowKey === row._id ? (
-                    <div>
-                      <button className="icon1" title="Save"
-                        onClick={() =>onSave(row._id, company, version, surveyNum)}>
-                        <GiIcons.GiSaveArrow />
-                      </button>
-                      <span className="slash" style={{ color: "#fff" }}>/</span>
-                      <button className="icon2" title="Cancel"
-                        onClick={() => onCancel()}>
-                        <MdIcons.MdCancel />
-                      </button>
-                    </div> */}
-                  {/* ) : ( */}
-                    <div>
-                      {/* <button className="icon3" title="Edit"
-                        onClick={() =>onEditClicked(row._id, row.company, row.version, row.surveyNumber)}>
-                        <BsIcons.BsPencilSquare />
-                      </button> */}
-                      <button className="icon3" title="Edit" onClick={()=>history.push(`/edit-survey/${row._id}`)}><BsIcons.BsPencilSquare /></button>
-                      <span className="slash" style={{ color: "#fff" }}>/</span> 
-                      <Link
-                        to="/create-new"
-                        className="icon5"
-                        title="Copy"
-                        style={{ color: "black" }}
-                        onClick={() => props.setRowId(row._id)}>
-                        <ImIcons.ImCopy />
-                      </Link>
-                      <span className="slash" style={{ color: "#fff" }}>/</span>
-                      <button className="icon4" title="Delete"
-                        onClick={() => handleDeleteClick(row._id)}>
-                        <RiIcons.RiDeleteBinFill />
-                      </button>
-                    </div>
-                  {/* )} */}
+                  <button 
+                    className="icon3" 
+                    title="Edit" 
+                    onClick={()=>history.push(`/edit-survey/${row._id}`)}>
+                    <BsIcons.BsPencilSquare />
+                  </button>
+                  <span className="slash" style={{ color: "#fff" }}>/</span> 
+                  <Link
+                    to="/create-new"
+                    className="icon5"
+                    title="Copy"
+                    style={{ color: "black" }}
+                    onClick={() => props.setRowId(row._id)}>
+                    <ImIcons.ImCopy />
+                  </Link>
+                  <span className="slash" style={{ color: "#fff" }}>/</span>
+                  <button className="icon4" title="Delete"
+                    onClick={() => handleDeleteClick(row._id)}>
+                    <RiIcons.RiDeleteBinFill />
+                  </button>
                 </td>
               </tr>
             );
