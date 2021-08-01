74c173b07012876b52ec6a8b2fa01f4488f4dd0b
Christy <57055910+christyc01@users.noreply.github.com>
2021-08-01 13:45:12 -0600

    Get edits to questionText saving to DB

--- client/src/components/AdminQuestions/SelectOne.js
+++ client/src/components/AdminQuestions/SelectOne.js
@@ -1,9 +1,13 @@
 import React, { useState, useContext, useEffect } from "react";
 import { QuestionContext } from "../pages/Admin/NewSurvey";
+import * as BsIcons from "react-icons/bs";
+import * as GiIcons from "react-icons/gi";
+import * as MdIcons from "react-icons/md";
 import * as RiIcons from "react-icons/ri";
 
 const SelectOne = ({ question, questionNumber}) => {
   const {questions, setQuestions} = useContext(QuestionContext)
+  const [inEditMode, setInEditMode] = useState({ status: false });
   const [questionText, setQuestionText]=useState(question.question || "In your opinion, what are the necessary and complementary organizational points for teleworking that should be implemented within the company? Many Answers are possible.\nPlease rank the following in order of interest:")
   const [answerOptions, setAnswerOptions]=useState(question.answerOptions ||
     [
@@ -19,6 +23,32 @@ const SelectOne = ({ question, questionNumber}) => {
   for (let i = 0; i < answerOptions.length; i++) {
     selectArray.push(num++);
   }
+
+
+  const onEditClicked = () => {
+    console.log("clicked edit");
+    setInEditMode({ status: true });
+  };
+
+  const onSave = () => {
+    setQuestions(questions);
+    setQuestionText(questionText)
+    //   const previousQuestions=questions
+    //   previousQuestions[questionNumber]={}
+    //   setQuestion(previousQuestions)
+    //   console.log("clicked save", questions);
+    setInEditMode({ status: false });
+  };
+
+  console.log("questions: ", questions)
+  console.log("questionText: ", questionText)
+
+  const onCancel = () => {
+    console.log("clicked cancel");
+    setInEditMode({ status: false });
+  };
+
+
 
   const onDelete = (e) => {
     e.preventDefault();
@@ -36,17 +66,77 @@ const SelectOne = ({ question, questionNumber}) => {
       answerOptions,
     };
     setQuestions(newQuestionList);
+  }, [questionText]);
+
+
+  const OnAddInput = () => {
+    console.log("clicked add");
+    setAnswerOptions([...answerOptions,
+      // radioOption
+    ]);
+    console.log("answer", answerOptions);
+    setInEditMode({ status: true });
+  };
+  const onInputChange = (event, index) => {
+    const previousAnswerOptions = answerOptions;
+    previousAnswerOptions[index] = event.target.value;
+    setAnswerOptions(previousAnswerOptions);
+  };
+
+  useEffect(() => {
+    const newQuestionList = [...questions];
+    newQuestionList[questionNumber - 1] = {
+      ...newQuestionList[questionNumber - 1],
+      question:questionText,
+      // questionNumber,
+      answerOptions,
+    };
+    setQuestions(newQuestionList);
   }, []);
+
 
   return (
     <div className="selectOne question-component admin-question-component">
       <button style={{ float: "right", width: "43px" }} onClick={onDelete}>
         <RiIcons.RiDeleteBinFill />
       </button>
+      <div className="edit-button">
+        <button
+          className="clear-icn3"
+          title="Edit"
+          onClick={() => onEditClicked()}
+        >
+          <BsIcons.BsPencilSquare />
+        </button>
+        <span className="slash" style={{ color: "#fff" }}>
+          /
+        </span>
+      </div>
+
+
       <p className="question-intro">Q{questionNumber}.</p>
-      <span>
-        <p className="question-intro">{questionText}</p>
-      </span>
+      {/* <span> */}
+        {inEditMode.status ? (
+
+          <textarea 
+            type="text"
+            className="question-intro"
+            value={questionText}
+            placeholder={questionText}
+            style={{height: "100px", width: "90%"}}
+            onChange={(e) => setQuestionText(e.target.value)}
+            />
+        ) 
+        :
+        (
+
+          <p className="question-intro">{questionText}</p>
+        )
+      
+      }
+      {/* </span> */}
+
+      {/* Add in inEditMode.status ? */}
       {answerOptions.map((row, i) => {
         return (
           <ul key={i}>
@@ -66,6 +156,55 @@ const SelectOne = ({ question, questionNumber}) => {
           </ul>
         );
       })}
+
+
+          {inEditMode.status ? (
+            <div className="edit-button">
+              <button
+                className="clear icn1"
+                title="Save"
+                onClick={() => onSave()}
+              >
+                <GiIcons.GiSaveArrow />
+              </button>
+              <span className="slash" style={{ color: "#fff" }}>
+                /
+              </span>
+              <button
+                className="clear icn2"
+                title="Cancel"
+                onClick={() => onCancel()}
+              >
+                <MdIcons.MdCancel />
+              </button>
+
+              <div className="edit-button">
+                <button
+                  className="clear icn4"
+                  title="Add"
+                  onClick={() => OnAddInput()}
+                >
+                  <BsIcons.BsFillPlusCircleFill />
+                </button>
+              </div>
+            </div>
+          ) : (
+            // </div>
+            <div className="edit-button">
+              {/* <button
+                className="clear icn3"
+                title="Edit"
+                onClick={() => onEditClicked()}
+              >
+                <BsIcons.BsPencilSquare />
+              </button> */}
+              <span className="slash" style={{ color: "#fff" }}>
+                /
+              </span>
+            </div>
+          )}
+        {/* </div> */}
+
     </div>
   );
 };
