04feb01a867930f67bf2fc962412eaf162169648
Tony Grimes <acidtone@tonygrimes.com>
2021-08-06 14:05:31 -0600

    added answer comments

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -12,6 +12,17 @@ router.get('/', async (req, res, next) => {
     const { survey } = req.query;
     let data = await Survey.find({ surveyNumber: survey}).exec();
     console.info(`records retrieved from mongoose:`, data?.length);
+
+    // Find the question number (or ID) with a question type of 'postal'
+    // 1. Loop through `data`
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+    // 2. Loop through each question object and add the first property where questionType == 'postal, to an array
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+
+    // 3. In Answer model find all answers for the above question ID. Place them in an array.
+
+    // Send postal codes back to the client
     res.send(data);
   });
 
