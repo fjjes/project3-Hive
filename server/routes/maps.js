efadd8d7d26c828140f9c448800be3841feebf5b
Prodyut Roy <prodyutroy2017@gmail.com>
2021-08-07 14:26:29 -0600

    Get Postal code Answers

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -10,20 +10,21 @@ const Answer = require('../models/Answer')
 // https://stackoverflow.blog/2020/03/02/best-practices-for-rest-api-design/#h-allow-filtering-sorting-and-pagination
 router.get('/', async (req, res, next) => {
     const { survey } = req.query;
-    let data = await Survey.find({ surveyNumber: survey}).exec();
-    console.info(`records retrieved from mongoose:`, data?.length);
-
+    let surveyData = await Survey.findOne({ surveyNumber: survey}).exec();
+    
     // Find the question number (or ID) with a question type of 'postal'
     // 1. Loop through `data`
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-
-    // 2. Loop through each question object and add the first property where questionType == 'postal, to an array
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
-
+    
+    const postalQuestion = surveyData.questions.find(element => element.questionType == 'postal');
+    console.log(postalQuestion)
     // 3. In Answer model find all answers for the above question ID. Place them in an array.
+    let answerData = await Answer.find({ survey: surveyData._id}).exec();
+
+    const postalAnswers = answerData.map(element => element.answers[postalQuestion.questionNumber]);
 
     // Send postal codes back to the client
-    res.send(data);
+    res.send(postalAnswers);
   });
 
 
