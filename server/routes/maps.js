67657576ce1527eb0d7c0843b6072c17196ef4ad
Prodyut Roy <prodyutroy2017@gmail.com>
2021-08-11 11:52:01 -0600

    added console log

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -8,15 +8,22 @@ const Answer = require('../models/Answer')
 // Create GET /maps?survey=[surveyNumber]
 router.get('/', async (req, res, next) => {
     const { survey } = req.query;
+    console.log(survey)
+  
     let surveyData = await Survey.findOne({ surveyNumber: survey}).exec();
+
+    console.log(surveyData)
     
     // Find the question number (or ID) with a question type of 'postal'
     // 1. Loop through `surveyData`
     
     const postalQuestion = surveyData.questions.find(element => element.questionType == 'postal');
+    console.log(postalQuestion)
     
     // 2. In Answer model find all answers for the above question ID. Place them in an array.
     let answerData = await Answer.find({ survey: surveyData._id}).exec();
+
+    console.log(answerData)
 
     const postalAnswers = answerData.map(element => element.answers[postalQuestion.questionNumber]);
 
