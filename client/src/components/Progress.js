7586f152cbafdd78bbd5204a52c92705e7f5a937
lucienlvmi <77748225+lucienlvmi@users.noreply.github.com>
2021-08-16 11:21:20 -0600

    progress bar changes color

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -2,9 +2,15 @@
 const Progress = ({done}) => {
     return (
         <div className="progress">
-            <div className="progress-done" style={{opacity: 1, width: `${done}%`}} >
-                {done}% complete
+            {done===0?
+            <div className="progress-done zero" style={{opacity: 1, width: `${done}%`}} >  
+            {done}% complete  
             </div>
+            :
+            <div className="progress-done" style={{opacity: 1, width: `${done}%`}} >  
+            {done}% complete  
+            </div>
+         }   
         </div>
     );
 }
