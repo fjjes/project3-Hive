3add6dbde11a8e657ce74b5e1c7a8b08691c6957
Prodyut Roy <prodyutroy2017@gmail.com>
2021-08-17 20:55:58 -0600

    added maps page filtered postal codes

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -9,26 +9,25 @@ const Answer = require('../models/Answer')
 router.get('/', async (req, res, next) => {
     const { surveyId } = req.query;
     console.log('Question Number: ', surveyId)
-  
     let surveyData = await Survey.findOne({ _id: surveyId}).exec();
-
     console.log('Survey Data: ', surveyData)
     
     // Find the question number (or ID) with a question type of 'postal'
     // 1. Loop through `surveyData`
-    
     const postalQuestion = surveyData.questions.find(element => element.questionType == 'postal');
-    console.log(postalQuestion)
-    
+    console.log('postal Question: ', postalQuestion)
+
     // 2. In Answer model find all answers for the above question ID. Place them in an array.
     let answerData = await Answer.find({ survey: surveyData._id}).exec();
-
     console.log('Answer Data: ', answerData)
+    const postalAnswers = answerData.map(element => element.answers[postalQuestion?.questionNumber]);
 
-    const postalAnswers = answerData.map(element => element.answers[postalQuestion.questionNumber]);
+    function removeFalsey(item) {
+      return (item ? true : false);
+    }
 
     // 3. Send postal codes back to the client
-    res.send(postalAnswers);
+    res.send(postalAnswers.filter(removeFalsey));
   });
 
 
