958f1a319d2fc628edb3828c8e11209ab18b7fa1
fjjes <fjjesreen@gmail.com>
2021-08-20 18:04:17 -0600

    checkbox and radio analysis table for charts

---
+++ client/src/components/DataVisual/AnalysisTable.js
@@ -0,0 +1,33 @@
+const AnalysisTable = ({xOptions, data, labels, question, qType}) => {
+    console.log(`${qType}:: xOptions`, xOptions)
+    console.log(`${qType}:: data`, data)
+    console.log(`${qType}:: question`, question)
+    console.log('------------------------------')
+
+    let obj = {}
+    xOptions.forEach((item, i)=>obj[item] = data[i])
+    console.log("obj::", obj)
+
+    function roundToTwo(num) {    
+        return +(Math.round(num + "e+2")  + "e-2");
+    }
+
+    return (
+        <div>
+            <table>
+                <tbody>
+                    {Object.keys(obj).map((itm, i)=>{
+                        return(
+                            <tr>
+                                <td>{itm}</td>
+                                <td>{`${roundToTwo(obj[itm])}%`}</td>
+                            </tr>
+                        )
+                    })}  
+                </tbody>
+            </table>
+        </div>
+    );
+}
+ 
+export default AnalysisTable;
