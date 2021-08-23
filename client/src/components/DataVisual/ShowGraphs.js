1e6427127178ee7322c204f7b36f3c1ecdaf279b
Nikita George <nikkigeorge2@gmail.com>
2021-08-23 13:23:27 -0600

    more styling on data analysis

--- client/src/components/DataVisual/ShowGraphs.js
+++ client/src/components/DataVisual/ShowGraphs.js
@@ -119,7 +119,7 @@ const ShowGraphs = ({question, qType,  answers, qNum, dataList, surveyId}) => {
             
                 {qType==='radio' ?
                 <div className='lower-sec'>
-                <div className="chart-container pie">
+							<div className="chart-container pie" style={{ marginRight: "200px" }}>
                     <Pie
                     data={{
                         // labels: dataList[0].survey.questions[qNum-1]?.answerOptions,
@@ -142,7 +142,7 @@ const ShowGraphs = ({question, qType,  answers, qNum, dataList, surveyId}) => {
                 
                 {qType==='checkbox' ?
                  <div className='lower-sec'>
-                    <div className="chart-container pie">
+							<div className="chart-container pie" style={{ marginRight: "200px" }}>
                         <Pie
                         data={{
                             labels: checkboxesAnswerOptions,
@@ -199,7 +199,7 @@ const ShowGraphs = ({question, qType,  answers, qNum, dataList, surveyId}) => {
 
                 {qType==='slider' ?
                  <div className='lower-sec'>
-                    <div className="chart-container pie">
+                    <div className="chart-container pie" style={{marginRight: "200px"}}>
                         <Pie
                         data={{
                             labels: sliderAnswerOptions,
